999cf04e7b251475fcecdf11aa6a436254db01bd
IOKLO <leesjung@gmail.com>
2024-12-02 16:42:19 +0900

    HelloWindow 예제

---
+++ C00_HelloWindow/main.cpp
@@ -0,0 +1,54 @@
+#include <Windows.h>
+#include "MyWindow.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+    MyWindow myWindow;
+
+    // 윈도우 클래스 등록
+    WNDCLASSEX windowClass = { 0 };
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = &MyWindow::WindowProc;
+    windowClass.hInstance = hInstance;
+    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    windowClass.lpszClassName = L"HelloWindow";
+    RegisterClassEx(&windowClass);
+
+    RECT windowRect{ 0, 0, 1280, 720 };
+    AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    // Create the window and store a handle to it.
+    HWND hWnd = CreateWindow(
+        windowClass.lpszClassName,
+        L"D3D12 Hello Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        windowRect.right - windowRect.left,
+        windowRect.bottom - windowRect.top,
+        nullptr,        // We have no parent window.
+        nullptr,        // We aren't using menus.
+        hInstance,
+        &myWindow);
+
+    myWindow.OnInit(hWnd);
+
+    ShowWindow(hWnd, nShowCmd);
+
+    // Main sample loop.
+    MSG msg = {};
+    while (msg.message != WM_QUIT)
+    {
+        // Process any messages in the queue.
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    myWindow.OnDestroy();
+
+    return (char)msg.wParam;
+}
